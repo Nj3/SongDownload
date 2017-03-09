8895ce7400f631cca239fcc4a024d669fb851345
Nj3 <natarajitachi@hotmail.com>
2017-03-09 07:56:28 +0530

    initial prototype completed

--- guitest.py
+++ guitest.py
@@ -1,13 +1,50 @@
 #! /usr/bin/env python3
 import tkinter as tk
 
+
+class songui(tk.Frame):
+
+    def __init__(self, master):
+        """creates a frame in a grid and creates widgets inside that frame"""
+        f = tk.Frame(master)
+        f.grid()
+        self.createwidgets(f)
+
+    def printsong(self):
+        print("Song name is %s - %s"%(self.e2.get(),self.e3.get()))
+
+    def createwidgets(self, f):
+        """3 labels, 3 Entry boxes, 2 buttons.
+        b1 = download button
+        b2 = quit button"""
+
+        #Labels
+        self.l1 = tk.Label(f, text = 'Language:')
+        self.l2 = tk.Label(f, text = 'Song Name:')
+        self.l3 = tk.Label(f, text = 'Singer/Movie Name:')
+
+        #Entry box
+        self.e1 = tk.Entry(f)
+        self.e2 = tk.Entry(f)
+        self.e3 = tk.Entry(f)
+        
+        #alignment in grid
+        self.l1.grid(row=0, sticky='e')
+        self.l2.grid(row=1, sticky='e')
+        self.l3.grid(row=2, sticky='e')
+
+        self.e1.grid(row=0, column=1)
+        self.e2.grid(row=1, column=1)
+        self.e3.grid(row=2, column=1)
+        
+        #Download button
+        self.b1 = tk.Button(f, text = 'Download', command = self.printsong)
+        self.b1.grid(row=3, column=0)
+        
+        #quit button
+        self.b2 = tk.Button(f, text = 'Quit', command = f.quit)
+        self.b2.grid(row=3, column=1)
+
 ui = tk.Tk()
-lbl = tk.Label(ui, text = 'Hello, can you hear me?')
-lbl.grid(row = 0, column = 0)
-
-ent1 = tk.Entry()
-ent1.grid(row = 0, column = 1)
-
-print(ent1.get())
-
+u = songui(ui)
 ui.mainloop()
--- main.py
+++ main.py
@@ -1,13 +1,67 @@
+#! /usr/bin/env python3
+
 #---------------------------importing libraries------------------------------#
+
 from __future__ import unicode_literals
 from bs4 import BeautifulSoup
 from urllib.request import urlopen,urlretrieve,Request
 import os
 from urllib.error import HTTPError,URLError
 import youtube_dl
+import tkinter as tk
+
+#----------------------------------GUI--------------------------------------#
+
+class songui(tk.Frame):
+
+    def __init__(self, master):
+        """creates a frame in a grid and creates widgets inside that frame"""
+        f = tk.Frame(master)
+        f.grid()
+        self.createwidgets(f)
+
+    def printsong(self):
+        print("Song name is %s - %s"%(self.e2.get(),self.e3.get()))
+
+    def createwidgets(self, f):
+        """3 labels, 3 Entry boxes, 2 buttons.
+        b1 = download button
+        z
+        b2 = quit button"""
+
+        #Labels
+        self.l1 = tk.Label(f, text = 'Language:')
+        self.l2 = tk.Label(f, text = 'Song Name:')
+        self.l3 = tk.Label(f, text = 'Singer/Movie Name:')
+
+        #Entry box
+        self.e1 = tk.Entry(f)
+        self.e2 = tk.Entry(f)
+        self.e3 = tk.Entry(f)
+        
+        #alignment in grid
+        self.l1.grid(row=0, sticky='e')
+        self.l2.grid(row=1, sticky='e')
+        self.l3.grid(row=2, sticky='e')
+
+        self.e1.grid(row=0, column=1)
+        self.e2.grid(row=1, column=1)
+        self.e3.grid(row=2, column=1)
+        
+        #Download button
+        self.b1 = tk.Button(f, text = 'Download', command = self.printsong)
+        self.b1.grid(row=3, column=0)
+        
+        #quit button
+        self.b2 = tk.Button(f, text = 'Quit', command = f.quit)
+        self.b2.grid(row=3, column=1)
+
+ui = tk.Tk()
+u = songui(ui)
+ui.mainloop()
 
 #----------------------------class declaration------------------------------------#
-class Songs(object):
+class Songs:
     """This class refers to a mp3 file which will contain all the methods to
     download, name, singer/movie name,..etc"""
 
@@ -16,6 +70,22 @@ class Songs(object):
         format: <singer/movie_name> - <song_name>
             ex: Linkin Park - Numb
         """
+    def play(self):
+        """Plays the downloaded song which is available in the music path to
+        play in rhythmbox if ubuntu or windows media player if windows"""
+
+    def saveloc(self):
+        """The location in our disk where the song will be saved.
+        Ubuntu: ~/Music
+        Windows: C:/Music
+        """
+
+    def language(self, lang):
+        """Sets the language of the song.
+        Used for creating folders while saving"""
+
+    def sites(self, lang):
+        """Based on language, sites from where we could download the songs"""
 
 
 
