c6c9ce68953caf3acc951d7c07744cc8caeb84ba
Natarajan Subramanian <natarajitachi@hotmail.com>
2017-04-03 08:02:53 +0530

    started tamil scrape, identified the source

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import os
 from urllib.error import HTTPError,URLError
 import youtube_dl
 import tkinter as tk
+import re
+import sys
 
 #----------------------------------GUI--------------------------------------#
 
@@ -87,47 +89,133 @@ class Songs:
     def sites(self, lang):
         """Based on language, sites from where we could download the songs"""
 
+#------------------------------------English Songs Scraping-----------------------------------#
 
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+    
+    def warning(self, msg):
+        print('warning msg: ', msg)
 
-def musicdl(dl_url,song_name):
-    """This function takes the URL and downloads the mp3 and save it in songs
-    folder. 
-    dl_url:  the url after scraping to the correct website to find the
-    mp3.
-    p: path where song will get saved.
-    song_name: The name of the song as entered by the user.
-    """
-    p = os.path.join(os.path.expanduser('~'),'Music/songtest')
-    if not os.path.exists(p):
-        os.mkdir(p)
-    try:
-        #urlretrieve(dl_url,p+'/'+song_name)
-        req = Request(dl_url, headers={'User-Agent':'Mozilla/5.0'})
-        tmp = urlopen(req)
-        with open(p+'/'+song_name+'.mp3', 'wb') as song:
-            song.write(tmp.read())
-    except HTTPError as e:
-        print('error code', e.code)
+    def error(self, msg):        
+        print('error msg: ', msg)
 
-def music_frm_youtube():
+def ytscrape(searchurl,baseurl):
+    """normal scraping"""
+    req = Request(searchurl, headers={'User-Agent':'Mozilla/5.0'})
+    lst[:] = []
+    url = urlopen(req)
+    soup = BeautifulSoup(url, 'lxml')
+    for i in soup.find_all('div',{'class':['yt-lockup-content','yt-lockup-meta-info']},limit=10):
+        for link,views in zip(i.select('h3 > a'),i.select('ul > li')):
+            if views is not None and views.next_sibling is not None:
+                lst.append([baseurl+link.get('href'),views.next_sibling.text])
+    for i in lst:
+        i[1] = int(re.sub(r' views|,','',i[1]))
+    lst.sort(key = lambda x:x[1])
+    url.close()
+    return lst[-1][0]
+
+def dl_frm_youtube(yt_lnk,dlpath):
     """passes the youtube url of the song. it extracts audio alone and saves it
     in local.
-    dl_url : youtube url for song which is priortised based on channel/views.
+    yt_lnk : youtube url for song which is priortised based on channel/views.
     """
-    ydl_opts = {'format':'bestaudio/best','postprocessors':[{'key':'FFmpegExtractAudio','preferredcodec':'mp3','preferredquality':'192',}]}
+    ydl_opts = {'format':'bestaudio/best','outtmpl':dlpath+'\\%(title)s.%(ext)s','postprocessors':[{'key':'FFmpegExtractAudio','preferredcodec':'mp3','preferredquality':'192',}],'logger': MyLogger()}
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download(['https://www.youtube.com/watch?v=gJeh_dLjPN4'])
-    print('download success')
+        #ydl.download([yt_lnk])
+        info = ydl.extract_info(yt_lnk, download=True)
+        songname = info.get('title', None)
+        #print(songname)
+        if os.path.isfile(dlpath+'\\'+songname+'.mp3'):
+            #print('found')
+            sys.exit(0)
 
-if __name__ == '__main__':
-    lang = input("Enter the language: ")
-    singer = input("Enter the author/movie name: ")
-    #dl_url ='https://www.yt-download.org/download/320-58b8d9af77ce1-10480000/mp3/PT2_F-1esPk/The%2BChainsmokers%2B-%2BCloser%2B%2528Lyric%2529%2Bft.%2BHalsey.mp3' 
-    song_name = input("Enter the song name: ")
-    if lang == "English":
-        site = ['mp3skull','beemp3','youtube']
-        music_frm_youtube()
-    elif lang == "Tamil":
-        site = ['freetamilmp3','tamilmp3world','youtube'],
+def beescrape(searchurl,baseurl,song,dlpath):
+    """scraping in beemp3s.org"""
+    req = Request(searchurl, headers={'User-Agent':'Mozilla/5.0'})
+    #print('inside beescrape now')
+    outerurl = urlopen(req)
+    lst[:] = []
+    outersoup = BeautifulSoup(outerurl,'lxml')
+    for i in outersoup.find_all('div',{'class':'item'},limit=5):
+        for link in i.select('div > a'):
+            if link.get('href').startswith('http:') and re.search('remix',link.get('href'),re.IGNORECASE) is None:
+                lst.append(link.get('href'))
+    outerurl.close()
+    for i in lst:
+        innerurl = urlopen(i)
+        innersoup = BeautifulSoup(innerurl,'lxml')
+        for link in innersoup.find_all('a',{'id':'download-button'}):
+            if link.get('href').endswith('.mp3') and link.get('href').startswith('http:') and re.search('remix',link.get('href'),re.IGNORECASE) is None:
+                urlretrieve(link.get('href'),dlpath+'\\'+song+'.mp3')
+                if os.path.isfile(dlpath+'\\'+song+'.mp3'):
+                    sys.exit(0)
+            innerurl.close()                    
+
+def main():
+    song = input('enter song name:')
+    artist = input('enter artist name:')
+    baseurl = ['https://www.youtube.com','http://beemp3s.org']
+    if sys.platform == 'win32':
+        dlpath = os.path.join(os.environ['USERPROFILE'],'Music','spd')
+        if not os.path.exists(dlpath):
+            os.mkdir(dlpath)
     else:
-        site = ['youtube','mp3skull']
+        dlpath = '~/Music/' + song + '.mp3'
+    searchurl = [baseurl[0] + '/results?search_query=' + '+' + artist.replace(chr(32),'+') + '+' + song.replace(chr(32),'+'), baseurl[1] +'/search?query=' + artist.replace(chr(32),'+') + '+' +song.replace(chr(32),'+') + '&field=all']
+    ytargs = (ytscrape(searchurl[0],baseurl[0]),dlpath)
+    beeargs = (searchurl[1],baseurl[1],song,dlpath)
+    args = [ytargs, beeargs]
+    scrapeit = [dl_frm_youtube, beescrape]
+    for func,arg in zip(scrapeit,args):
+        func(*arg)
+
+
+lst = []
+main()
+
+
+#def musicdl(dl_url,song_name):
+#    """This function takes the URL and downloads the mp3 and save it in songs
+#    folder. 
+#    dl_url:  the url after scraping to the correct website to find the
+#    mp3.
+#    p: path where song will get saved.
+#    song_name: The name of the song as entered by the user.
+#    """
+#    p = os.path.join(os.path.expanduser('~'),'Music/songtest')
+#    if not os.path.exists(p):
+#        os.mkdir(p)
+#    try:
+#        #urlretrieve(dl_url,p+'/'+song_name)
+#        req = Request(dl_url, headers={'User-Agent':'Mozilla/5.0'})
+#        tmp = urlopen(req)
+#        with open(p+'/'+song_name+'.mp3', 'wb') as song:
+#            song.write(tmp.read())
+#    except HTTPError as e:
+#        print('error code', e.code)
+#
+#def music_frm_youtube():
+#    """passes the youtube url of the song. it extracts audio alone and saves it
+#    in local.
+#    dl_url : youtube url for song which is priortised based on channel/views.
+#    """
+#    ydl_opts = {'format':'bestaudio/best','postprocessors':[{'key':'FFmpegExtractAudio','preferredcodec':'mp3','preferredquality':'192',}]}
+#    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+#        ydl.download(['https://www.youtube.com/watch?v=gJeh_dLjPN4'])
+#    print('download success')
+#
+#if __name__ == '__main__':
+#    lang = input("Enter the language: ")
+#    singer = input("Enter the author/movie name: ")
+#    #dl_url ='https://www.yt-download.org/download/320-58b8d9af77ce1-10480000/mp3/PT2_F-1esPk/The%2BChainsmokers%2B-%2BCloser%2B%2528Lyric%2529%2Bft.%2BHalsey.mp3' 
+#    song_name = input("Enter the song name: ")
+#    if lang == "English":
+#        site = ['mp3skull','beemp3','youtube']
+#        music_frm_youtube()
+#    elif lang == "Tamil":
+#        site = ['freetamilmp3','tamilmp3world','youtube'],
+#    else:
+#        site = ['youtube','mp3skull']
---
+++ scraptest_tamil.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen,urlretrieve,Request
+import os
+import youtube_dl
+import re
+import sys
+
+def tamildl():
+    """custom search in google using `site:freetamilmp3.in` trick"""
+
+
+def main():
+    song = input('enter the song name:')
+    movie = input('enter the movie name:')
+    baseurl = 'https://www.google.co.in/#spf=1'
+    searchurl = ''
