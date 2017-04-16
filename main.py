2f3e4f0ced325c3ae1bc82fcb57bf16995dcb65d
Natarajan Subramanian <natarajitachi@hotmail.com>
2017-04-16 08:36:44 +0530

    basic refactoring of class is done

--- main.py
+++ main.py
@@ -13,6 +13,190 @@ import sys
 import google
 from selenium import webdriver
 
+#----------------------------class declaration------------------------------------#
+class Songs:
+    """This class refers to a mp3 file which will contain all the methods to
+    download, name, singer/movie name,..etc"""
+
+    def __init__(self, lang, song_nm, movie_name, artist_name):
+        """Initializes the class with name and singer/movie name in the below
+        format: <singer/movie_name> - <song_name>
+            ex: Linkin Park - Numb
+        """
+        if lang == 'English':
+            self.lang = lang
+            self.song_nm = song_nm
+            self.singer = artist_name
+        else:
+            self.lang = lang
+            self.song_nm = song_nm
+            self.singer = movie_name
+        self.saveloc
+        self.dl_sites = []
+        self.sites
+
+    def __repr__(self):
+        """prints out the string representation of song object"""
+        return 'The song name is %r from %r'%(self.song_nm, self.singer)
+
+    def saveloc(self):
+        """The location in our disk where the song will be saved.
+        Ubuntu: ~/Music/SD/<lang>/singer/*
+        Windows: C:/Music/SD/<lang>/singer/*
+        """
+        if sys.platform == 'win32':
+            dlpath = os.path.join(os.environ['USERPROFILE'], 'Music', 'SD', self.lang, self.singer)
+            if not os.path.exists(dlpath):
+                os.mkdir(dlpath)
+        else:
+            dlpath = os.path,join(os.path.expanduser('~'), 'Music', 'SD', self.lang, self.singer)
+            if not os.path.exists(dlpath):
+                os.mkdir(dlpath)
+        self.dlpath = dlpath
+        return
+    
+
+    def sites(self):
+        """Based on language, sites from where we could download the songs.
+        Tamil: freetamilmp3.in, youtube
+        English:youtube, beemp3s.org"""
+        if self.lang == 'English':
+            self.dl_sites = ['https://www.youtube.com','http://beemp3s.org']
+        else:
+            self.dl_sites = ['http://freetamilmp3.in/','https://www.youtube.com']
+        return
+ 
+
+
+##------------------------------------English Songs Scraping-----------------------------------#
+#
+#class MyLogger(object):
+#    def debug(self, msg):
+#        pass
+#    
+#    def warning(self, msg):
+#        print('warning msg: ', msg)
+#
+#    def error(self, msg):        
+#        print('error msg: ', msg)
+#
+#def ytscrape(searchurl,baseurl):
+#    """normal scraping"""
+#    req = Request(searchurl, headers={'User-Agent':'Mozilla/5.0'})
+#    lst[:] = []
+#    url = urlopen(req)
+#    soup = BeautifulSoup(url, 'lxml')
+#    for i in soup.find_all('div',{'class':['yt-lockup-content','yt-lockup-meta-info']},limit=10):
+#        for link,views in zip(i.select('h3 > a'),i.select('ul > li')):
+#            if views is not None and views.next_sibling is not None:
+#                lst.append([baseurl+link.get('href'),views.next_sibling.text])
+#    for i in lst:
+#        i[1] = int(re.sub(r' views|,','',i[1]))
+#    lst.sort(key = lambda x:x[1])
+#    url.close()
+#    return lst[-1][0]
+#
+#def dl_frm_youtube(yt_lnk,dlpath):
+#    """passes the youtube url of the song. it extracts audio alone and saves it
+#    in local.
+#    yt_lnk : youtube url for song which is priortised based on channel/views.
+#    """
+#    ydl_opts = {'format':'bestaudio/best','outtmpl':dlpath+'\\%(title)s.%(ext)s','postprocessors':[{'key':'FFmpegExtractAudio','preferredcodec':'mp3','preferredquality':'192',}],'logger': MyLogger()}
+#    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+#        #ydl.download([yt_lnk])
+#        info = ydl.extract_info(yt_lnk, download=True)
+#        songname = info.get('title', None)
+#        #print(songname)
+#        if os.path.isfile(dlpath+'\\'+songname+'.mp3'):
+#            #print('found')
+#            sys.exit(0)
+#
+#def beescrape(searchurl,baseurl,song,dlpath):
+#    """scraping in beemp3s.org"""
+#    req = Request(searchurl, headers={'User-Agent':'Mozilla/5.0'})
+#    #print('inside beescrape now')
+#    outerurl = urlopen(req)
+#    lst[:] = []
+#    outersoup = BeautifulSoup(outerurl,'lxml')
+#    for i in outersoup.find_all('div',{'class':'item'},limit=5):
+#        for link in i.select('div > a'):
+#            if link.get('href').startswith('http:') and re.search('remix',link.get('href'),re.IGNORECASE) is None:
+#                lst.append(link.get('href'))
+#    outerurl.close()
+#    for i in lst:
+#        innerurl = urlopen(i)
+#        innersoup = BeautifulSoup(innerurl,'lxml')
+#        for link in innersoup.find_all('a',{'id':'download-button'}):
+#            if link.get('href').endswith('.mp3') and link.get('href').startswith('http:') and re.search('remix',link.get('href'),re.IGNORECASE) is None:
+#                urlretrieve(link.get('href'),dlpath+'\\'+song+'.mp3')
+#                if os.path.isfile(dlpath+'\\'+song+'.mp3'):
+#                    sys.exit(0)
+#            innerurl.close()                    
+#
+#def main():
+#    song = input('enter song name:')
+#    artist = input('enter artist name:')
+#    baseurl = ['https://www.youtube.com','http://beemp3s.org']
+#    if sys.platform == 'win32':
+#        dlpath = os.path.join(os.environ['USERPROFILE'],'Music','spd')
+#        if not os.path.exists(dlpath):
+#            os.mkdir(dlpath)
+#    else:
+#        dlpath = '~/Music/' + song + '.mp3'
+#    searchurl = [baseurl[0] + '/results?search_query=' + '+' + artist.replace(chr(32),'+') + '+' + song.replace(chr(32),'+'), baseurl[1] +'/search?query=' + artist.replace(chr(32),'+') + '+' +song.replace(chr(32),'+') + '&field=all']
+#    ytargs = (ytscrape(searchurl[0],baseurl[0]),dlpath)
+#    beeargs = (searchurl[1],baseurl[1],song,dlpath)
+#    args = [ytargs, beeargs]
+#    scrapeit = [dl_frm_youtube, beescrape]
+#    for func,arg in zip(scrapeit,args):
+#        func(*arg)
+#
+#
+#lst = []
+#main()
+#
+##----------------------------------------------Tamil songs------------------------------------------------#
+#
+#def tamildl(searchquery, dlpath, song):
+#    """custom search in google using `site:freetamilmp3.in` trick"""
+#    baseurl = 'http://freetamilmp3.in/'
+#    lst[:] = []
+#    for url in google.search(searchquery, tld='co.in',lang='eng',start = 0, stop = 5):
+#        lst.append(url)
+#    for url in lst:
+#        driver = webdriver.PhantomJS()
+#        driver.get(url)
+#        urlsrc = driver.page_source.encode('utf-8')
+#        soup = BeautifulSoup(urlsrc, 'lxml')
+#        for link in soup.find_all('a'):
+#            if re.search('Full mp3',link.text,re.IGNORECASE) is not None:
+#                if link.get('href').startswith('http:'):
+#                    dllink = link.get('href')
+#                else:
+#                    dllink = urllib.parse.urljoin(baseurl,link.get('href'))
+#                req = requests.get(dllink)
+#                urlretrieve(urllib.parse.quote(dllink,safe='/|:'), os.path.join(dlpath,song+'.mp3'))
+#                if os.path.isfile(os.path.join(dlpath,song+'.mp3')):
+#                    sys.exit(0)
+#
+#def main():
+#    song = input('enter the song name:')
+#    movie = input('enter the movie name:')
+#    if sys.platform == 'win32':
+#        dlpath = os.path.join(os.environ['USERPROFILE'],'Music','spd')
+#        if not os.path.exists(dlpath):
+#            os.mkdir(dlpath)
+#    else:
+#        dlpath = '~/Music/' + song + '.mp3'
+#    tamildl(searchquery,dlpath,song)
+#
+#lst = []
+#main()
+
+def main(lang, song, mov, artist):
+    """this function is the main controller for calling different function"""
+    print('successfully called main fn %s,%s,%s,%s'%(lang, song, mov, artist))
+
 #----------------------------------GUI--------------------------------------#
 
 class SongUI(tk.Frame): #pylint: disable=too-many-ancestors
@@ -43,6 +227,8 @@ class SongUI(tk.Frame): #pylint: disable=too-many-ancestors
             """This function will get triggered after user select a value in drop down.
             Based on this value, it will create widgets"""
             print(value)
+            #set language back to variable so that we can use it later.
+            self.langvar.set(value)
             #song row 
             self.lbl_song.grid(row=1, column=0, sticky=tk.E, pady=(20, 0))
             self.entry_song.grid(row=1, column=1, columnspan=2, pady=(20, 0))
@@ -54,8 +240,7 @@ class SongUI(tk.Frame): #pylint: disable=too-many-ancestors
                 #singer row
                 self.lbl_movie.grid(row=2, column=0, sticky=tk.E)
                 self.entry_movie.grid(row=2, column=1, columnspan=2)
-            
-            #download button alignment after everything is created
+           #download button alignment after everything is created
             self.button_dl.grid(row=3, column=0, pady=(30, 0), columnspan=3)
         
         #required labels
@@ -84,6 +269,7 @@ class SongUI(tk.Frame): #pylint: disable=too-many-ancestors
     def printsong(self):
         print('GUI working successfully')
         print(self.entry_song.get(), self.entry_movie.get(), self.entry_artist.get())
+        main(self.langvar.get(),self.entry_song.get(), self.entry_movie.get(), self.entry_artist.get())
 
 
 root = tk.Tk()
@@ -91,150 +277,4 @@ root.resizable(width=False, height=False)
 app = SongUI(master=root)
 app.mainloop()
 
-#----------------------------class declaration------------------------------------#
-class Songs:
-    """This class refers to a mp3 file which will contain all the methods to
-    download, name, singer/movie name,..etc"""
 
-    def __init__(self, name, singer):
-        """Initializes the class with name and singer/movie name in the below
-        format: <singer/movie_name> - <song_name>
-            ex: Linkin Park - Numb
-        """
-    def saveloc(self):
-        """The location in our disk where the song will be saved.
-        Ubuntu: ~/Music
-        Windows: C:/Music
-        """
-
-    def language(self, lang):
-        """Sets the language of the song.
-        Used for creating folders while saving"""
-
-    def sites(self, lang):
-        """Based on language, sites from where we could download the songs"""
-
-#------------------------------------English Songs Scraping-----------------------------------#
-
-class MyLogger(object):
-    def debug(self, msg):
-        pass
-    
-    def warning(self, msg):
-        print('warning msg: ', msg)
-
-    def error(self, msg):        
-        print('error msg: ', msg)
-
-def ytscrape(searchurl,baseurl):
-    """normal scraping"""
-    req = Request(searchurl, headers={'User-Agent':'Mozilla/5.0'})
-    lst[:] = []
-    url = urlopen(req)
-    soup = BeautifulSoup(url, 'lxml')
-    for i in soup.find_all('div',{'class':['yt-lockup-content','yt-lockup-meta-info']},limit=10):
-        for link,views in zip(i.select('h3 > a'),i.select('ul > li')):
-            if views is not None and views.next_sibling is not None:
-                lst.append([baseurl+link.get('href'),views.next_sibling.text])
-    for i in lst:
-        i[1] = int(re.sub(r' views|,','',i[1]))
-    lst.sort(key = lambda x:x[1])
-    url.close()
-    return lst[-1][0]
-
-def dl_frm_youtube(yt_lnk,dlpath):
-    """passes the youtube url of the song. it extracts audio alone and saves it
-    in local.
-    yt_lnk : youtube url for song which is priortised based on channel/views.
-    """
-    ydl_opts = {'format':'bestaudio/best','outtmpl':dlpath+'\\%(title)s.%(ext)s','postprocessors':[{'key':'FFmpegExtractAudio','preferredcodec':'mp3','preferredquality':'192',}],'logger': MyLogger()}
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        #ydl.download([yt_lnk])
-        info = ydl.extract_info(yt_lnk, download=True)
-        songname = info.get('title', None)
-        #print(songname)
-        if os.path.isfile(dlpath+'\\'+songname+'.mp3'):
-            #print('found')
-            sys.exit(0)
-
-def beescrape(searchurl,baseurl,song,dlpath):
-    """scraping in beemp3s.org"""
-    req = Request(searchurl, headers={'User-Agent':'Mozilla/5.0'})
-    #print('inside beescrape now')
-    outerurl = urlopen(req)
-    lst[:] = []
-    outersoup = BeautifulSoup(outerurl,'lxml')
-    for i in outersoup.find_all('div',{'class':'item'},limit=5):
-        for link in i.select('div > a'):
-            if link.get('href').startswith('http:') and re.search('remix',link.get('href'),re.IGNORECASE) is None:
-                lst.append(link.get('href'))
-    outerurl.close()
-    for i in lst:
-        innerurl = urlopen(i)
-        innersoup = BeautifulSoup(innerurl,'lxml')
-        for link in innersoup.find_all('a',{'id':'download-button'}):
-            if link.get('href').endswith('.mp3') and link.get('href').startswith('http:') and re.search('remix',link.get('href'),re.IGNORECASE) is None:
-                urlretrieve(link.get('href'),dlpath+'\\'+song+'.mp3')
-                if os.path.isfile(dlpath+'\\'+song+'.mp3'):
-                    sys.exit(0)
-            innerurl.close()                    
-
-def main():
-    song = input('enter song name:')
-    artist = input('enter artist name:')
-    baseurl = ['https://www.youtube.com','http://beemp3s.org']
-    if sys.platform == 'win32':
-        dlpath = os.path.join(os.environ['USERPROFILE'],'Music','spd')
-        if not os.path.exists(dlpath):
-            os.mkdir(dlpath)
-    else:
-        dlpath = '~/Music/' + song + '.mp3'
-    searchurl = [baseurl[0] + '/results?search_query=' + '+' + artist.replace(chr(32),'+') + '+' + song.replace(chr(32),'+'), baseurl[1] +'/search?query=' + artist.replace(chr(32),'+') + '+' +song.replace(chr(32),'+') + '&field=all']
-    ytargs = (ytscrape(searchurl[0],baseurl[0]),dlpath)
-    beeargs = (searchurl[1],baseurl[1],song,dlpath)
-    args = [ytargs, beeargs]
-    scrapeit = [dl_frm_youtube, beescrape]
-    for func,arg in zip(scrapeit,args):
-        func(*arg)
-
-
-lst = []
-main()
-
-#----------------------------------------------Tamil songs------------------------------------------------#
-
-def tamildl(searchquery, dlpath, song):
-    """custom search in google using `site:freetamilmp3.in` trick"""
-    baseurl = 'http://freetamilmp3.in/'
-    lst[:] = []
-    for url in google.search(searchquery, tld='co.in',lang='eng',start = 0, stop = 5):
-        lst.append(url)
-    for url in lst:
-        driver = webdriver.PhantomJS()
-        driver.get(url)
-        urlsrc = driver.page_source.encode('utf-8')
-        soup = BeautifulSoup(urlsrc, 'lxml')
-        for link in soup.find_all('a'):
-            if re.search('Full mp3',link.text,re.IGNORECASE) is not None:
-                if link.get('href').startswith('http:'):
-                    dllink = link.get('href')
-                else:
-                    dllink = urllib.parse.urljoin(baseurl,link.get('href'))
-                req = requests.get(dllink)
-                urlretrieve(urllib.parse.quote(dllink,safe='/|:'), os.path.join(dlpath,song+'.mp3'))
-                if os.path.isfile(os.path.join(dlpath,song+'.mp3')):
-                    sys.exit(0)
-
-def main():
-    song = input('enter the song name:')
-    movie = input('enter the movie name:')
-    if sys.platform == 'win32':
-        dlpath = os.path.join(os.environ['USERPROFILE'],'Music','spd')
-        if not os.path.exists(dlpath):
-            os.mkdir(dlpath)
-    else:
-        dlpath = '~/Music/' + song + '.mp3'
-    tamildl(searchquery,dlpath,song)
-
-lst = []
-main()
